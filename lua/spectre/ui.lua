f355d90dc3df0d462e8580d49e2de4f0905f57eb
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-25 20:47:25 +0700

    removed line number and column

--- lua/spectre/ui.lua
+++ lua/spectre/ui.lua
@@ -82,7 +82,7 @@ M.render_filename = function(bufnr, namespace, line, entry)
         state.user_config.color_devicons, '+')
 
     api.nvim_buf_set_lines(state.bufnr, line, line, false, {
-        string.format("%s %s%s:%s:%s:", icon, directory, filename, entry.lnum, entry.col),
+        string.format("%s %s%s", icon, directory, filename),
     })
 
     local width = vim.api.nvim_strwidth(filename)
